8054e34a22e51cd622f9754affd327475559d58e
theblackminilik <126102366+theblackminilik@users.noreply.github.com>
2023-02-24 08:42:28 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
